8a142037e208237d3eea704be3be8ed388bf8376
Kartik Nagila <60644731+cybermonkey14@users.noreply.github.com>
2020-03-25 13:33:02 +0530

    Update Corona_cases_notifier.py

--- Corona_cases_notifier.py
+++ Corona_cases_notifier.py
@@ -11,13 +11,6 @@ def notifyMe(title,message):
 		app_icon = os.getcwd()+"\covid.ico",
 		timeout = 15
 		)
-
-print("Welcome to the program.\n")
-print("Do you wanna pop up the program in specific time intervals or one time?\n")
-ask = int(input("enter 1 for one time or 2  for time interval: "))
-ti_me = int(input("\nEnter the amount of interval in minutes which the program should pop up: "))
-amount = ti_me*60
-
 global total_case,total_death,total_rec
 def getdata():
         r = requests.get("https://virusncov.com/")
@@ -26,13 +19,18 @@ def getdata():
         getdata.total_case = "Total Corona cases: %s " %(soup.h2.text.split(" ")[-1])+"\n"
         getdata.total_death = "Total Deaths: %s " %(soup.find("span",{"class":"red-text"}).text+"\n")
         getdata.total_rec = "Total Recovered: %s " %(soup.find("span",{"class":"green-text"}).text)
-        
+
+print("Welcome to the program.\n")
+print("Do you wanna pop up the program in specific time intervals or one time?\n")
+ask = int(input("enter 1 for one time or 2  for time interval any other key to exit: "))
 
 if ask == 1:
         print("\nfetching data\n")
         getdata()
         notifyMe("Corona stats",total_case+total_death+total_rec)
 elif ask == 2:
+        ti_me = int(input("\nEnter the amount of interval in minutes which the program should pop up: "))
+        amount = ti_me*60
         while True:
                 getdata()
                 notifyMe("Corona stats",getdata.total_case+getdata.total_death+getdata.total_rec)
